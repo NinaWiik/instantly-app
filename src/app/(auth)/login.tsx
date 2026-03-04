5b2f7debe1f127d1a3e747844673e7edd0c48a7d
Nina Wiik <ninawiik1@outlook.com>
2026-03-04 15:47:25 +0100

    Added styling to the signup and login page

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,19 +1,27 @@
+import { useRouter } from "expo-router";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen() {
+ const router = useRouter();
+
+ const handleSignIn = () => {
+  router.push("/(auth)/signup");
+ };
+
  return (
-  <SafeAreaView edges={["top", "bottom"]}>
-   <View>
-    <Text>Welcome back!</Text>
-    <Text className="text-2xl font-bold">Sign in to your account</Text>
-    <View>
+  <SafeAreaView edges={["top", "bottom"]} className="flex-1">
+   <View className="flex-1 justify-center p-6">
+    <Text className="text-3xl font-bold mb-2">Welcome back!</Text>
+    <Text className="text-xl mb-6 text-gray-500">Sign in to your account</Text>
+    <View className="w-full">
      <TextInput
       placeholder="Email"
       placeholderTextColor="gray"
       keyboardType="email-address"
       autoComplete="email"
       autoCapitalize="none"
+      className="border border-gray-300 rounded-md p-4 mb-4"
      />
      <TextInput
       placeholder="Password"
@@ -21,12 +29,19 @@ export default function LoginScreen() {
       autoComplete="password"
       secureTextEntry={true}
       autoCapitalize="none"
+      className="border border-gray-300 rounded-md p-4 mb-4"
      />
-     <TouchableOpacity>
-      <Text>Sign in</Text>
+     <TouchableOpacity className="bg-black rounded-md p-4 mb-4 items-center justify-center">
+      <Text className="text-white font-bold text-md">Sign in</Text>
      </TouchableOpacity>
-     <TouchableOpacity>
-      <Text>Don&apos;t have an account? Sign up</Text>
+     <TouchableOpacity
+      onPress={handleSignIn}
+      className="items-center justify-center text-xs"
+     >
+      <Text className="text-gray-600 text-sm">
+       Don&apos;t have an account?{" "}
+       <Text className="text-black font-bold">Sign up</Text>
+      </Text>
      </TouchableOpacity>
     </View>
    </View>
---
+++ src/app/(auth)/signup.tsx
@@ -0,0 +1,50 @@
+import { useRouter } from "expo-router";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function SignupScreen() {
+ const router = useRouter();
+
+ const handleSignIn = () => {
+  router.push("/(auth)/login");
+ };
+
+ return (
+  <SafeAreaView edges={["top", "bottom"]} className="flex-1">
+   <View className="flex-1 justify-center p-6">
+    <Text className="text-3xl font-bold mb-2">Create an account</Text>
+    <Text className="text-xl mb-6 text-gray-500">Sign up to get started</Text>
+    <View className="w-full">
+     <TextInput
+      placeholder="Email"
+      placeholderTextColor="gray"
+      keyboardType="email-address"
+      autoComplete="email"
+      autoCapitalize="none"
+      className="border border-gray-300 rounded-md p-4 mb-4"
+     />
+     <TextInput
+      placeholder="Password"
+      placeholderTextColor="gray"
+      autoComplete="password"
+      secureTextEntry={true}
+      autoCapitalize="none"
+      className="border border-gray-300 rounded-md p-4 mb-4"
+     />
+     <TouchableOpacity className="bg-black rounded-md p-4 mb-4 items-center justify-center">
+      <Text className="text-white font-bold text-md">Sign up</Text>
+     </TouchableOpacity>
+     <TouchableOpacity
+      onPress={handleSignIn}
+      className="items-center justify-center text-xs"
+     >
+      <Text className="text-gray-600 text-sm">
+       Don&apos;t have an account?{" "}
+       <Text className="text-black font-bold">Sign in</Text>
+      </Text>
+     </TouchableOpacity>
+    </View>
+   </View>
+  </SafeAreaView>
+ );
+}
